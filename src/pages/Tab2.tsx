fc1af8a34c2a02259c0fe7191dd995cd6694f9f7
Carlos Pazmiño <crpazmino@puce.edu.ec>
2026-06-16 15:03:00 -0500

    feat: sistema de usuarios con roles y permisos implementado

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,22 +1,46 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+import React from 'react';
 
 const Tab2: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Formulario de repositorio</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        
+        <div className="container">
+          <IonInput
+            className="form-field"
+            label="Nombre del repositorio"
+            placeholder="Ingrese el nombre del repositorio"
+            labelPlacement="floating"
+          />
+
+          <IonTextarea
+            className="form-field"
+            label="Descripción del repositorio"
+            placeholder="Ingrese la descripción del repositorio"
+            labelPlacement="floating"
+            rows={4}
+          />
+
+          <IonButton
+            className="save-button"
+            expand="block"
+            fill="solid"
+          >
+            Guardar
+          </IonButton>
+        </div>
       </IonContent>
     </IonPage>
   );
--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab3.css';
 
 const Tab3: React.FC = () => {
@@ -7,16 +6,29 @@ const Tab3: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 3</IonTitle>
+          <IonTitle>Perfil de usuario</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
+            <IonTitle size="large">Perfil de usuario</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+        <div className="container">
+          <IonCard className="card">
+            <img
+              src="https://avatars.githubusercontent.com/u/173958328?v=4"
+              alt="Foto de perfil"
+            />
+            <IonCardTitle>Carlos Pazmino Zambrano</IonCardTitle>
+            <IonCardSubtitle>crpazmino@puce.edu.ec</IonCardSubtitle>
+            <IonCardContent>
+              <p>Desarrollador de software formado en la Pontificia Universidad Católica del Ecuador.</p>
+              <p>Si deseas contactarme, buscarme buscame en google</p>
+            </IonCardContent>
+          </IonCard>
+        </div>
       </IonContent>
     </IonPage>
   );
